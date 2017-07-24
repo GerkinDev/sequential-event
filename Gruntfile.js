e3f792a17575dad3e87bb5c443e3259e7e8d3c4e
Alexandre Germain <agermain@student.42.fr>
2017-07-24 12:58:17 +0200

    Updated pipelines

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,12 @@ module.exports = function gruntInit( grunt ) {
 	// Project configuration.
 
 	const baseDocPath = 'docs';
-	const jsAssets = [
-		'Gruntfile.js',
+	const jsLib = [
 		'lib/**.js',
 	];
+	const jsAssets = _.concat([
+		'Gruntfile.js',
+	], jsLib );
 
 	grunt.initConfig({
 		pkg:    grunt.file.readJSON( 'package.json' ),
@@ -32,23 +34,10 @@ module.exports = function gruntInit( grunt ) {
 						expand: true,
 						src:    [
 							'dist/**/*.js',
-							'!**/*.min.js'
+							'!**/*.min.js',
 						],
 						cwd:    '.',
 						rename: ( dst, src ) => src.replace( /.js$/, '.min.js' ),
-					},
-				],
-			},
-		},
-		copy: {
-			dist: {
-				files: [
-					{
-						expand: true,
-						src:    [ '**/*.js' ],
-						cwd:    'js/dependencies',
-						dest:   'dist/dependencies',
-						//rename: ( dst, src ) => dst + '/' + src.replace( /(\.min)?.js$/, '.min.js' ),
 					},
 				],
 			},
@@ -58,36 +47,23 @@ module.exports = function gruntInit( grunt ) {
 				format: 'stylish', //'node_modules/eslint-tap',
 				fix:    true,
 			},
-			info: {
-				options: {
-					configFile: 'eslint-es6-browser.json',
-					silent:     true,
-				},
-				src: jsAssets,
-			},
 			strict: {
 				options: {
-					configFile: 'eslint-es6-browser.json',
+					configFile: 'eslint-es6.json',
 				},
 				src: jsAssets,
 			},
 		},
-		docco_husky: {
-			files: {
-				expand: true,
-				src:    jsAssets,
-			},
-			project_name: 'AltCore',
-			output_dir:   `${ baseDocPath }/docco`,
-		},
 		jsdoc: {
-			src:     jsAssets,
-			options: {
-				private:     true,
-				destination: `${ baseDocPath }/jsdoc`,
-				config:	     'jsdoc.json',
-				template:    './node_modules/ink-docstrap/template',
-				readme:      'README-jsdoc.md',
+			main: {
+				src:     jsLib,
+				options: {
+					private:     true,
+					destination: `${ baseDocPath }`,
+					config:	     'jsdoc.json',
+					template:    './node_modules/ink-docstrap/template',
+					readme:      'README.md',
+				},
 			},
 		},
 		babel: {
@@ -97,125 +73,36 @@ module.exports = function gruntInit( grunt ) {
 			},
 			dist: {
 				files: [{
-					'expand': true,
-					'cwd':    'js/',
-					'src':    [
-						'**/*.js',
-						'!dependencies/**/*.js',
-					],
-					'dest': 'dist/',
-					'ext':  '.js',
+					expand: true,
+					cwd:    '.',
+					src:    jsLib,
+					dest:   'dist/',
+					ext:    '.js',
 				}],
-			},
-		},
-		lesslint: {
-			info: _.merge({}, lesslint, {
-				options: {
-					failOnWarning: false,
-				},
-			}),
-			strict: _.merge({}, lesslint, {
-				options: {
-					failOnWarning: true,
-				},
-			}),
-		},
-		less: {
-			dist: {
-				files:   lessFiles,
-				options: {
-					plugins: [
-						new ( require( 'less-plugin-autoprefix' ))({
-							browsers: 'last 2 versions',
-						}), // add vendor prefixes
-						new ( require( 'less-plugin-clean-css' ))({
-							advanced: true,
-						}),
-					],
-				},
-			},
-		},
-		markdown: {
-			index: {
-				files: [
-					{
-						src:  'README.md',
-						dest: 'docs/index.html',
-						ext:  '.html',
-					},
-				],
-				options: {
-					//template: 'myTemplate.jst',
-					preCompile: function( src, context ) {
-					},
-					postCompile: function( src, context ) {
-					},
-					templateContext:    {},
-					contextBinder:      false,
-					contextBinderMark:  '@@@',
-					autoTemplate:       true,
-					autoTemplateFormat: 'jst',
-					markdownOptions:    {
-						gfm:       true,
-						highlight: 'manual',
-						codeLines: {
-							before: '<span>',
-							after:  '</span>',
-						},
-					},
-				},
 			},
 		},
 	});
 
 	// Load the plugin that provides the 'uglify' task.
 	grunt.loadNpmTasks( 'grunt-jsdoc' );
-	grunt.loadNpmTasks( 'grunt-docco-husky' );
-	grunt.loadNpmTasks( 'grunt-markdown' );
+	grunt.loadNpmTasks( 'grunt-babel' );
 	grunt.loadNpmTasks( 'gruntify-eslint' );
-	grunt.loadNpmTasks( 'grunt-lesslint' );
-	grunt.loadNpmTasks( 'grunt-contrib-less' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 
 	require( 'load-grunt-tasks' )( grunt );
 
 	grunt.registerTask(
 		'documentate',
 		[
-			'markdown:index',
-			'jsdoc',
-			'docco_husky',
+			'jsdoc:main',
 		]
 	);
 	grunt.registerTask(
-		'refreshStyles',
+		'dist',
 		[
-			'lesslint:info',
-			'less:dist',
-		]
-	);
-	grunt.registerTask(
-		'refreshScripts',
-		[
-			'eslint:info',
+			'eslint:strict',
 			'babel:dist',
 			'uglify:dist',
-			'copy:dist',
-		]
-	);
-	grunt.registerTask(
-		'refreshResources',
-		[
-			'refreshStyles',
-			'refreshScripts',
-		]
-	);
-	grunt.registerTask(
-		'lint',
-		[
-			'eslint:info',
-			'lesslint:info',
 		]
 	);
 };
