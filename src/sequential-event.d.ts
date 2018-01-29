c42095e991c5284cad7b48694f2f47c4d5f3e837
Gerkin <nihilivin@gmail.com>
2018-01-29 02:46:02 +0100

    To ts

--- src/sequential-event.d.ts
+++ src/sequential-event.d.ts
@@ -1,20 +1,23 @@
 //import * as SequentialEvent from './sequential-event';
 
 declare namespace SequentialEvent {
-  export interface IEventHandler {
-    (...args: any[]): Promise<any> | any
-  }
-  export interface IEventsHash {
-    [key: string]: IEventHandler[]
-  }
-  export interface IOnceHandler extends IEventHandler {
-    origFn: IEventHandler
-  }
+	export interface IEventHandler {
+		(...args: any[]): Promise<any> | any
+	}
+	export interface IEventsHash {
+		[key: string]: IEventHandler[]
+	}
+	export interface IEventHash {
+		[key: string]: IEventHandler | IEventHandler[]
+	}
+	export interface IOnceHandler extends IEventHandler {
+		origFn: IEventHandler
+	}
 }
 
 declare module 'sequential-event' {
-  import SE = SequentialEvent
-  export = SE
+	import SE = SequentialEvent
+	export = SE
 }
 
 export = SequentialEvent
