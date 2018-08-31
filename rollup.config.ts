0010692ca06e39826798bb952d9722e2b8956ac0
Gerkin <nihilivin@gmail.com>
2018-08-31 21:07:05 +0200

    build(rollup): Replaced rollup-plugin-uglify with rollup-plugin-terser
    rollup-plugin-terser is built for es6+ bundles, while rollup-plugin-uglify is built for es5

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,37 +3,37 @@ import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import uglify from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser'
 
 const pkg = require('./package.json')
 
 const libraryName = 'sequential-event'
 
 export default {
-  input: `src/${libraryName}.ts`,
-  output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd' },
-    { file: pkg.module, name: camelCase(libraryName), format: 'es' },
-  ],
-  sourcemap: true,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    // Compile TypeScript files
-    typescript(),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Minify
-    uglify(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+	input: `src/${libraryName}.ts`,
+	output: [
+		{ file: pkg.main, name: camelCase(libraryName), format: 'umd' },
+		{ file: pkg.module, name: camelCase(libraryName), format: 'es' },
+	],
+	sourcemap: true,
+	// Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+	external: [],
+	watch: {
+		include: 'src/**',
+	},
+	plugins: [
+		// Compile TypeScript files
+		typescript(),
+		// Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+		commonjs(),
+		// Minify
+		terser(),
+		// Allow node_modules resolution, so you can use 'external' to control
+		// which external modules to include in the bundle
+		// https://github.com/rollup/rollup-plugin-node-resolve#usage
+		resolve(),
+		
+		// Resolve source maps to the original source
+		sourceMaps(),
+	],
 }
